585a446df17730fe265a7eeb6c875ad3b8d16d7b
caiopeternela <caiopeternela.dev@gmail.com>
2023-10-21 15:14:22 -0300

    finishing the web project

--- cn/web-project/app.js
+++ cn/web-project/app.js
@@ -2,10 +2,11 @@ const express = require("express")
 const app = express()
 
 app.set("view engine", "ejs")
+app.set("trust proxy", true)
 
 app.get("/", (req, res) => {
-    date = new Intl.DateTimeFormat("en-US", { dateStyle: "full", timeStyle: "long", timeZone: "America/Sao_Paulo" }).format(new Date()),
-    res.render("index", { date: date })
+    date = new Intl.DateTimeFormat("en-US", { dateStyle: "full", timeStyle: "long", timeZone: "America/Sao_Paulo" }).format(new Date())
+    res.render("index", { date: date, ip: req.ip })
 })
 
 app.listen(3000, () => {
